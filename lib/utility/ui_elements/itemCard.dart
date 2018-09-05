c46a3afc076b8fb2ce6d411ab29b21a1ed5dfe89
hptg1994 <hptg1994@gmail.com>
2018-09-05 18:13:56 -0400

    add a bottom action

--- lib/utility/ui_elements/itemCard.dart
+++ lib/utility/ui_elements/itemCard.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
 
 class ItemCard extends StatelessWidget {
+  Image _getImage() {}
+
+  Widget _buildItemInfo() {
+    return Container(
+        padding: EdgeInsets.symmetric(vertical: 2.0, horizontal: 3.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+//            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              new IconButton(
+                icon: Icon(Icons.favorite),
+                color: Colors.grey,
+              ),
+              new IconButton(
+                icon: Icon(Icons.comment),
+                color: Colors.grey,
+              )
+            ],
+          ),
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 5.0,
       child: Column(
+//        crossAxisAlignment: CrossAxisAlignment.stretch,
+//        mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Container(
-            width: 30.0,
-            height: 40.0,
-            decoration: BoxDecoration(color: Colors.grey),
-            margin: EdgeInsets.all(5.0),
-          )
+          new Expanded(child: new Align(
+            alignment: Alignment.bottomCenter,
+            child: _buildItemInfo(),
+          )),
         ],
       ),
     );
