63e53a777b3979fc448a7d3cd545eab4039107c2
hptg1994 <hptg1994@gmail.com>
2018-09-06 14:43:30 -0400

    adding detail page

--- lib/pages/ItemDetailPage/ItemDetailPage.dart
+++ lib/pages/ItemDetailPage/ItemDetailPage.dart
@@ -28,11 +28,42 @@ class ItemDetailPage extends StatelessWidget {
               ),
             ),
           ),
-          SliverList(delegate: SliverChildListDelegate([
-            Container(
-              decoration: BoxDecoration(color: Colors.pink),
-            )
-          ]),)
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Container(
+                  padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 0.0),
+                  child: new Column(
+//                    mainAxisAlignment: MainAxisAlignment.center,
+//                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Icon(Icons.favorite_border, color: Colors.grey),
+                          Icon(Icons.comment, color: Colors.grey),
+                          Icon(Icons.remove_red_eye, color: Colors.grey)
+                        ],
+                      ),
+                      SizedBox(height: 10.0),
+                      ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: null,
+                        ),
+                        title: Text("Name of the item"),
+                        subtitle: Text("Auther"),
+                      ),
+                      SizedBox(height: 10.0),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 0.0,horizontal: 20.0),
+                        child: Text("Item description,")
+
+                      )
+
+                    ],
+                  )
+              )
+            ]),
+          )
         ],
       ),
       floatingActionButton: FloatingButtons(),
